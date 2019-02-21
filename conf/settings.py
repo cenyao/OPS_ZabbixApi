acc9c5f6b5d086cfe528eeb0cce595d6fb02fd28
tianruocen <tianruocen@haowan123.com>
2019-02-21 16:14:08 +0800

    修改配置文件

--- conf/settings.py
+++ conf/settings.py
@@ -6,15 +6,10 @@ import logging
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # zabbix的API地址、用户名、密码、这里修改为实际的参数
-# ZABBIX_URL = "http://192.168.25.100/zabbix/api_jsonrpc.php"
-# ZABBIX_HEADER = {"Content-Type": "application/json"}
-# ZABBIX_USER = "Admin"
-# ZABBIX_PASS = "zabbix"
-
-ZABBIX_URL="http://monitor-1.haowan123.com/api_jsonrpc.php"
-ZABBIX_HEADER = {"Content-Type":"application/json"}
-ZABBIX_USER = "admin"
-ZABBIX_PASS = "!prZiq>H@^>gvW:H<p"
+ZABBIX_URL = "http://192.168.25.100/zabbix/api_jsonrpc.php"
+ZABBIX_HEADER = {"Content-Type": "application/json"}
+ZABBIX_USER = "Admin"
+ZABBIX_PASS = "zabbix"
 
 LOG_LEVEL = logging.INFO
 LOG_TYPES = {
